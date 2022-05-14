18c6d630593817619e573672b3bb74f6d18e52e6
Methanon Keaokrachang <48640302+MarkystarkDotCO@users.noreply.github.com>
2022-05-14 13:49:05 +0700

    Create LED_with_button.c

---
+++ LED_with_button.c
@@ -0,0 +1,47 @@
+
+int led1=5;
+int led2=6;
+int Direction=0;
+int freq = 100;
+void setup() {
+  for(int i=2; i<10; i++){
+     pinMode(i, OUTPUT);
+     digitalWrite(i,LOW);
+    }
+}
+
+void loop() {
+
+ 
+    if(led1>2 && Direction==0){
+    digitalWrite(led1,HIGH);
+    digitalWrite(led2,HIGH);
+    delay(freq);
+    digitalWrite(led1,LOW);
+    digitalWrite(led2,LOW);
+    delay(freq);
+    led1--;
+    led2++;
+    freq = freq*0.6;
+    if(led1==2){Direction=1;}
+    
+    }
+   
+    if(led1>=2 && Direction==1){
+    digitalWrite(led1,HIGH);
+    digitalWrite(led2,HIGH);
+    delay(freq);
+    digitalWrite(led1,LOW);
+    digitalWrite(led2,LOW);
+    delay(freq);
+    led1++;
+    led2--;
+    freq = freq*0.6;
+    if(led1==5){
+      Direction=0;
+      freq = 100;
+      }
+    }
+
+  
+}
